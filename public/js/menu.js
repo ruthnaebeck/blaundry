80d4bd1c84140b222a0c100ff08613f9211c44d7
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-04-27 18:09:02 -0400

    Font size changes

--- public/js/menu.js
+++ public/js/menu.js
@@ -12,11 +12,11 @@ BearClothes.Menu.prototype = {
     this.background = this.game.add.sprite(0, 0, 'sky')
     //start game text
     var text = 'Help the Bear Gather as Many Clothes as Possible!'
-    var style = { font: '30px Arial', fill: '#fff', align: 'center' }
+    var style = { font: '25px Arial', fill: '#fff', align: 'center' }
     var bText = this.game.add.text(this.game.width / 2, this.game.height / 2, text, style)
     bText.anchor.set(0.5)
     text = 'Click to Start'
-    style = { font: '20px Arial', fill: '#fff', align: 'center' }
+    style = { font: '15px Arial', fill: '#fff', align: 'center' }
     var cText = this.game.add.text(this.game.width / 2, this.game.height / 2 + 30, text, style)
     cText.anchor.set(0.5)
     text = 'Highest score: ' + this.highestScore
